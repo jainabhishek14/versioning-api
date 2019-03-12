10103e2d43e7eced8339392ca02c32a08df1c890
Abhishek Jain <abhishek.jain@wns.com>
2019-03-12 13:01:30 +0530

    bypass ssl verification for token validation endpoint

--- config/params.js
+++ config/params.js
@@ -3,5 +3,6 @@
 module.exports = {
 	authenticationUrl: process.env.AUTH_URI || "https://gw-wns.com/api/dev/ms-pyt-validatetoken-0011/1/validatetoken/",
 	unsecuredPath: ["/health", "/logs"],
+	sslVerify: false,
 	limitNumResults : 100
 };
